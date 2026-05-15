57d2ab845c3e4872633172a4f8fd654d7d85b499
GeeanneGerona <git config user.email geeanne-email@gmail.com>
2026-05-15 19:19:26 +0800

    feat: add product reviews and ratings by GeeanneGerona

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Seller\ProductController as SellerProductController;
 use App\Http\Controllers\Buyer\ProductController as BuyerProductController;
 use App\Http\Controllers\Buyer\CartController;
 use App\Http\Controllers\Buyer\OrderController;
+use App\Http\Controllers\Buyer\ReviewController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 Route::get('/', function () {
@@ -32,6 +33,8 @@ Route::middleware(['auth'])->prefix('buyer')->name('buyer.')->group(function () 
     Route::resource('products', BuyerProductController::class)->only(['index', 'show']);
     Route::resource('cart', CartController::class)->only(['index', 'store', 'destroy']);
     Route::resource('orders', OrderController::class)->only(['index', 'store']);
+    Route::patch('orders/{order}/cancel', [OrderController::class, 'cancel'])->name('orders.cancel');
+    Route::post('products/{product}/review', [ReviewController::class, 'store'])->name('products.review');
 });
 
 // Admin Routes
